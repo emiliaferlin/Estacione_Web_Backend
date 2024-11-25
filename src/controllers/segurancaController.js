ddfb46c8ae92d4e69c50463b027f93c71ee7b8c8
Emilia <emilia.ferlin@sbsistemas.com.br>
2024-11-25 10:43:57 -0300

    chore: ajusta variavel de tempo

--- src/controllers/segurancaController.js
+++ src/controllers/segurancaController.js
@@ -6,7 +6,7 @@ const login = async (request, response) => {
     await autenticaUsuarioDB(request.body)
         .then(usuario => {
             const token = jwt.sign({ usuario }, process.env.SECRET, {
-                expiresIn: 1800 // 30 minutos
+                expiresIn: 10 // 30 minutos
             })
             return response.json({ auth: true, token: token })
         })
